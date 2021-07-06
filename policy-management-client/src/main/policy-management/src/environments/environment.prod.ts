18520c97f4d06f601af8056160aef4d955364c37
Ramchandra Shilawane <akshay.ramchandra-shilawane@capgemini.com>
2021-07-06 13:17:16 +0530

    tomcat port number changes

--- policy-management-client/src/main/policy-management/src/environments/environment.prod.ts
+++ policy-management-client/src/main/policy-management/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   environmentName: 'Dev',
-  authApi: 'http://localhost:8080/policy-management-server/api/auth/',
-  policyApi: "http://localhost:8080/policy-management-server/api/policy/",
-  userApi:"http://localhost:8080/policy-management-server/api/"
+  authApi: 'http://localhost:8084/policy-management-server/api/auth/',
+  policyApi: "http://localhost:8084/policy-management-server/api/policy/",
+  userApi:"http://localhost:8084/policy-management-server/api/"
 };
